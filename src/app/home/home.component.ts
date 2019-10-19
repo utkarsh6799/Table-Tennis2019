cb399f9b65b07a398ff7b30de9c8029366de7f93
utkarsh6799 <utkarshsingh6799@gmail.com>
2019-10-19 19:20:58 +0530

    final code 2.0

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import Parallax from 'parallax-js';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  
+  constructor() {
+    
+   }
 
   ngOnInit() {
+    let scene = document.getElementById('scene');
+    var parallaxInstance = new Parallax(scene, {
+      hoverOnly: true
+    });
+
+    let scene1 = document.getElementById('scene1');
+    var parallaxInstance = new Parallax(scene1, {
+      hoverOnly: true
+    });
+
+    let scene2 = document.getElementById('scene2');
+    var parallaxInstance = new Parallax(scene2, {
+      hoverOnly: true
+    });
+
+    let scene3 = document.getElementById('scene3');
+    var parallaxInstance = new Parallax(scene3, {
+      hoverOnly: true
+    });
   }
 
 }
